aeb082406ba1db89d31c36da3b369269a858d949
Pavel <shved64@mail.ru>
2024-08-27 16:15:44 +0400

    3.2. Загрузка файлов в github [#243562]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Pavel Kravchuk");
+        System.out.println("05.08.1990");
+    }
+}
