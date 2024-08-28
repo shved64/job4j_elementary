9499571b13c206185a7db6e5bbe13f3b88e04ae5
Pavel <shved64@mail.ru>
2024-08-28 15:38:38 +0400

    3.2. Загрузка файлов в github [#243562]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+
+    public static double sumSubstactAndDivide(double first, double second) {
+        return subtract(first, second)
+                + divide(first, second);
+    }
+
+    public static double sumAllFunction(double first, double second) {
+        return subtract(first, second)
+                + divide(first, second)
+                 + multiply(first, second)
+                  + sum(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + sumSubstactAndDivide(20, 10));
+        System.out.println("Результат расчета равен: " + sumAllFunction(5, 5));
+    }
+}
