5e34395364b2c49912f5fdea84237f8db036f0e0
Pavel <shved64@mail.ru>
2024-08-29 12:21:53 +0400

    3.2. Загрузка файлов в github [#243562]

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CounterTest {
+
+    @Test
+    void whenSumFrom0To5Then15() {
+    int start = 0;
+    int finish = 5;
+    int expected = 15;
+    int result = Counter.sum(start, finish);
+    assertEquals(expected, result);
+    }
+
+    @Test
+    void whenSumFrom3To8Then33() {
+        int start = 3;
+        int finish = 8;
+        int expected = 33;
+        int result = Counter.sum(start, finish);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void whenSumFrom1To1Then1() {
+        int start = 1;
+        int finish = 1;
+        int expected = 1;
+        int result = Counter.sum(start, finish);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void whenSumFrom3To2Then0() {
+        int start = 3;
+        int finish = 2;
+        int expected = 0;
+        int result = Counter.sum(start, finish);
+        assertEquals(expected, result);
+    }
+}
