c003f431ff164de4d76befd0ee4830e5bf785072
Pavel <shved64@mail.ru>
2024-08-30 10:54:20 +0400

    3.2. Загрузка файлов в github [#243562]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -5,9 +5,9 @@ public class Factorial {
     public static int calculate(int number) {
         int calculate = 1;
         for (int i = 2; i <= number; i++) {
-        calculate = calculate * i;
-    }
+            calculate = calculate * i;
+        }
         return calculate;
     }
-    }
+}
 
