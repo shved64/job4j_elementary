28b54b3e358b00f426063da39f905dd7fb16e776
Pavel <shved64@mail.ru>
2024-08-30 16:45:37 +0400

    3.2. Загрузка файлов в github [#243562]

---
+++ src/main/java/ru/job4j/loop/NumberPyramid.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+public class NumberPyramid {
+    public static void draw(int n) {
+        for (int i = 1; i <= n; i++) {
+
+            for (int k = n; k > i; k--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Number pyramid of height 3:");
+        draw(3);
+        System.out.println("Number pyramid of height 5:");
+        draw(5);
+    }
+}
