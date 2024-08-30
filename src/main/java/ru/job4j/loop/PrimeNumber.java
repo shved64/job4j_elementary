bfb067b0b5353d108fa55f4612dc2a230d60c0a1
Pavel <shved64@mail.ru>
2024-08-30 16:16:26 +0400

    3.2. Загрузка файлов в github [#243562]

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -5,22 +5,11 @@ public class PrimeNumber {
     public static int calc(int finish) {
         int count = 0;
         for (int i = 2; i <= finish; i++) {
-            if (check(i)) {
+            if (CheckPrimeNumber.check(i)) {
                 count++;
             }
         }
         return count;
     }
 
-    public static boolean check(int number) {
-        boolean prime = number > 1;
-        for (int i = 2; i <= number / 2; i++) {
-
-            if (number % i == 0) {
-                return false;
-            }
-
-        }
-        return prime;
-    }
 }
