2aca84aa07fa10ae7183c3a8beb543c54ad1ab1c
Pavel <shved64@mail.ru>
2024-08-30 16:32:58 +0400

    3.2. Загрузка файлов в github [#243562]

--- src/main/java/ru/job4j/loop/Frame.java
+++ src/main/java/ru/job4j/loop/Frame.java
@@ -7,7 +7,7 @@ public class Frame {
                 boolean isTopOrBottomBorder = row == 0 || row == size - 1;
                 boolean isSideBorder = col == 0 || col == size - 1;
                 if (isTopOrBottomBorder && isSideBorder) {
-                    System.out.print("+");
+                    System.out.print("+"); // ”глы рамки
                 } else if (isTopOrBottomBorder) {
                     System.out.print("-");
                 } else if (isSideBorder) {
