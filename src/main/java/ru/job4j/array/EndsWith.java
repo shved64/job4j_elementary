5d6fe4eca9beac2d42f6ccbb3948374cc171392b
Pavel <shved64@mail.ru>
2024-09-02 12:23:48 +0400

    3.2. Загрузка файлов в github [#243562]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] postfix) {
+        for (int i = 0; i < postfix.length; i++) {
+            if (word[word.length - i - 1] != postfix[postfix.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
