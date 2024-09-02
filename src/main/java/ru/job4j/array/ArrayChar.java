df0919d870375f3759c940e2c569faef7bde0614
Pavel <shved64@mail.ru>
2024-09-02 11:43:29 +0400

    3.2. Загрузка файлов в github [#243562]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,13 +3,12 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] prefix) {
-        boolean result = true;
         for (int i = 0; i < prefix.length; i++) {
                 if (word[i] != prefix[i]) {
                     return false;
                 }
             }
 
-            return result;
+            return true;
         }
     }
