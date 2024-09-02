ebefd2cf002b59229683882efb0223769d0b3c63
Pavel <shved64@mail.ru>
2024-09-02 11:40:44 +0400

    3.2. Загрузка файлов в github [#243562]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,12 +3,11 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        boolean first = data[0];
         for (int i = 1; i < data.length; i++) {
-            if (data[i] != first) {
+            if (data[i] != data[0]) {
                 result = false;
+                break;
             }
-            break;
 
         }
         return result;
