c2bb5018f282972a5da1b7897fca79b0f0d4081d
Pavel <shved64@mail.ru>
2024-09-05 18:25:13 +0400

    3.2. Загрузка файлов в github [#243562]

--- src/test/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/test/java/ru/job4j/array/SimpleStringEncoder.java
@@ -23,7 +23,7 @@ public class SimpleStringEncoder {
             }
         }
         result = appendEncoded(result, symbol, counter);
-                return result;
+        return result;
     }
 }
 
