3a7dedc5327893c7b95e56cfca68000dfaecb7a8
Pavel <shved64@mail.ru>
2024-09-09 16:35:28 +0400

    3.2. Загрузка файлов в github [#243562]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -12,6 +12,7 @@ public class AlgoArray {
         array[3] = array[4];
         array[4] = temp;
         for (int index = 0; index < array.length; index++) {
+
             System.out.println(array[index]);
         }
     }
